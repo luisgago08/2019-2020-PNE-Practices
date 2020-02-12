fd1355ad55df3321427337e0f654fc65df1a0817
luisgago <l.garciago.2019@alumnos.urjc.es>
2020-02-12 18:30:43 +0100

    Exercise 3

---
+++ P0/Ex3.py
@@ -0,0 +1,7 @@
+from Seq0 import *
+
+print('-----| Exercise 3 |------')
+
+list_ = ['U5', 'RNU6_269P']
+
+print('Gene U5 ---> Length:', seq_len(list))
--- P0/Seq0.py
+++ P0/Seq0.py
@@ -13,3 +13,15 @@ def seq_read_fasta(filename):
     seq_dna = seq_dna.replace('\n', '')
     seq_dna = seq_dna[:20]
     return seq_dna
+
+
+def seq_len(list):
+    for element in list:
+        element = element + '.txt'
+        file_contents = Path(element).read_text()
+        seq_dna = file_contents
+        index_finish = seq_dna.find('\n')
+        seq_dna = seq_dna[index_finish + 1:]
+        seq_dna = seq_dna.replace('\n', '')
+        number_bases = len(seq_dna)
+        return number_bases
