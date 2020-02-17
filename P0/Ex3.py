87b6f3077b97020e13631e03b364ce18784ebbec
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-02-17 23:06:42 +0100

    ex 3

--- P0/Ex3.py
+++ P0/Ex3.py
@@ -1,7 +1,20 @@
 from Seq0 import *
+from pathlib import Path
 
 print('-----| Exercise 3 |------')
 
+FOLDER = '../Session-04/'
+Names = ['U5.txt', 'RNU6_269P.txt', 'FXN.txt', 'ADA.txt']
 
+# for filename in Names:
+    # file_contents = Path(FOLDER + filename).read_text()
+    # seq_dna = file_contents
+    # index_finish = seq_dna.find('\n')
+    # seq_dna = seq_dna[index_finish + 1:]
+    # seq_dna = seq_dna.replace('\n', '')
+    # number_bases = len(seq_dna)
+    # print(number_bases)
 
-print('Gene U5 ---> Length:', seq_len(list))
+# print('Gene U5 ---> Length:', seq_len(FOLDER + Names[0]))
+
+print(seq_len(FOLDER + filename))
--- P0/Seq0.py
+++ P0/Seq0.py
@@ -15,13 +15,12 @@ def seq_read_fasta(filename):
     return seq_dna
 
 
-def seq_len(list):
-    for element in list:
-        element = element + '.txt'
-        file_contents = Path(element).read_text()
+def seq_len(a):
+    for filename in a:
+        file_contents = Path(FOLDER + filename).read_text()
         seq_dna = file_contents
         index_finish = seq_dna.find('\n')
         seq_dna = seq_dna[index_finish + 1:]
         seq_dna = seq_dna.replace('\n', '')
         number_bases = len(seq_dna)
-        return number_bases
+        return(number_bases)
