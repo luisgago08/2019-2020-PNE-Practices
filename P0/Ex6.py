a75c0a3eac027afe58c589ccb2b6417d04125414
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-02-25 23:53:42 +0100

    Ex 6

---
+++ P0/Ex6.py
@@ -0,0 +1,7 @@
+from Seq0 import *
+
+FOLDER = '../Session-04/'
+Names = ['U5.txt', 'RNU6_269P.txt', 'FXN.txt', 'ADA.txt', 'FRAT1.txt']
+Bases = ['A', 'C', 'T', 'G']
+
+print('-----| Exercise 5 |------')
