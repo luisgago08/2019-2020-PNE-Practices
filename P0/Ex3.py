8f29e8412754a218758bbbc9c7639504904de167
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-02-25 21:52:11 +0100

    Ex 3

--- P0/Ex3.py
+++ P0/Ex3.py
@@ -1,9 +1,10 @@
 from Seq0 import *
-from pathlib import Path
-
-print('-----| Exercise 3 |------')
 
 FOLDER = '../Session-04/'
 Names = ['U5.txt', 'RNU6_269P.txt', 'FXN.txt', 'ADA.txt']
 
-print(seq_len(FOLDER + filename))
+print('-----| Exercise 3 |------')
+
+for gene in Names:
+    seq = seq_read_fasta(FOLDER + gene)
+    print(f"Gene {gene} ---> Length: {seq_len(seq)}")
