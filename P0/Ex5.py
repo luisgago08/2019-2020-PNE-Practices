cab10c3c22db01dc02bc145cb01d596d56407e00
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-02-25 23:40:13 +0100

    Ex 5

--- P0/Ex5.py
+++ P0/Ex5.py
@@ -4,11 +4,7 @@ FOLDER = '../Session-04/'
 Names = ['U5.txt', 'RNU6_269P.txt', 'FXN.txt', 'ADA.txt', 'FRAT1.txt']
 Bases = ['A', 'C', 'T', 'G']
 
-print('-----| Exercise 4 |------')
-
+print('-----| Exercise 5 |------')
 for gene in Names:
     seq = seq_read_fasta(FOLDER + gene)
-    print()
-    print(f"Gene {gene}:")
-    for base in Bases:
-        print(f"  {base}: {seq_count_base(seq, base)}")
+    print(f"Gene {gene}: {seq_count(seq)}")
