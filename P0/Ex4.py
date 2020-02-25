e2e26ad3bcab1656afe32d01d5a2caa671ae6837
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-02-25 22:05:26 +0100

    Ex 4

--- P0/Ex4.py
+++ P0/Ex4.py
@@ -1,2 +1,14 @@
-from seq0 import *
+from Seq0 import *
 
+FOLDER = '../Session-04/'
+Names = ['U5.txt', 'RNU6_269P.txt', 'FXN.txt', 'ADA.txt']
+Bases = ['A', 'C', 'T', 'G']
+
+print('-----| Exercise 4 |------')
+
+for gene in Names:
+    seq = seq_read_fasta(FOLDER + gene)
+    print()
+    print(f"Gene {gene}:")
+    for base in Bases:
+        print(f"  {base}: {seq_count_base(seq, base)}")
