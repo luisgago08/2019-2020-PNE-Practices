a87dea4a9610916153a091a7eb32d621d0e597f7
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-02-26 10:21:14 +0100

    Ex 6

--- P0/Ex5.py
+++ P0/Ex5.py
@@ -5,9 +5,3 @@ Names = ['U5.txt', 'RNU6_269P.txt', 'FXN.txt', 'ADA.txt', 'FRAT1.txt']
 Bases = ['A', 'C', 'T', 'G']
 
 print('-----| Exercise 6 |------')
-GENE = Names[0]
-print(f"Gene {GENE}:")
-seq = seq_read_fasta(FOLDER + Names[0])[:20]
-rev = seq_reverse(seq)
-print(f"Frag: {seq}")
-print(f"Rev : {rev}")
