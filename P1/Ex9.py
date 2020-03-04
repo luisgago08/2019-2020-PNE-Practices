e7203c26ef4494ac65c41383142b851899551626
luisgago <l.garciago.2019@alumnos.urjc.es>
2020-03-04 18:52:34 +0100

    Ex 8

--- P1/Ex9.py
+++ P1/Ex9.py
@@ -1,15 +1,9 @@
 from Seq1 import Seq
 
+FOLDER = '../Session-04/'
+Names = ['U5', 'RNU6_269P', 'FXN', 'ADA', 'FRAT1']
+Bases = ['A', 'C', 'T', 'G']
+txt = '.txt'
+
 print(f"-----| Exercise 9 |------")
 
-s1 = Seq()
-
-s2 = Seq("ACTGA")
-
-s3 = Seq("Invalid sequence")
-
-for i, s in enumerate([s1, s2, s3]):
-    print(f"Sequence {i}: (Length: {s.len()}) {s}")
-    print(f"Bases{s.count()}")
-    print(f"Rev: {s.reverse()}")
-    print(f"Comp: {s.complement()}")
