e19d3ee45453367ef21447e54bfcebe7ef734301
luisgago <l.garciago.2019@alumnos.urjc.es>
2020-03-04 18:47:45 +0100

    Ex 8

--- P1/Ex8.py
+++ P1/Ex8.py
@@ -12,3 +12,4 @@ for i, s in enumerate([s1, s2, s3]):
     print(f"Sequence {i}: (Length: {s.len()}) {s}")
     print(f"Bases{s.count()}")
     print(f"Rev: {s.reverse()}")
+    print(f"Comp: {s.complement()}")
