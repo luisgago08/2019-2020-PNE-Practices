a17cd24e31a5cc2d8892ba9e81ac0aaac985d9bb
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-05-22 01:25:23 +0200

    Practice 4

---
+++ P4/Ex5.py
@@ -0,0 +1,108 @@
+import socket
+from pathlib import Path
+
+# -- Server network parameters
+IP = "127.0.0.1"
+PORT = 8080
+
+
+def get_resource(path):
+    response = ""
+    if path == "/info/A":  # Lo que aparece en el URL
+        # This new contents are written in HTML language
+        response = Path("A.html").read_text()
+
+    elif path == "/info/C":  # Lo que aparece en el URL
+        response = Path("C.html").read_text()
+
+    elif path == "/info/T":  # Lo que aparece en el URL
+        response = Path("T.html").read_text()
+
+    elif path == "/info/G":  # Lo que aparece en el URL
+        response = Path("G.html").read_text()
+
+    elif path != "/info/T" and path != "/info/C" and path != "/info/C" and path != "/info/A":
+        response = Path("Error.html").read_text()
+    return response
+
+
+def process_client(s):
+    # -- Receive the request message
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    # -- Split the request messages into lines
+    lines = req.split('\n')
+
+    # -- The request line is the first
+    req_line = lines[0]
+
+    print("Request line: ", end="")
+    print(req_line)
+
+    # Process the request line
+    words = req_line.split(" ")
+
+    # Get the method and path
+    method = words[0]
+    path = words[1]  # El path es a partir de la barra en el URL, después IP, PORT
+
+    print(f"Method: {method}")
+    print(f"Path: {path}")
+
+    # Response body
+    # Initially it is blank
+
+    body = ""
+
+    if method == "GET":
+        body = get_resource(path)
+
+    # -- Status line: We respond that everything is ok (200 code)
+    status_line = "HTTP/1.1 200 OK\n"
+
+    # -- Add the Content-Type header
+    header = "Content-Type: text/html\n"
+
+    # -- Add the Content-Length
+    header += f"Content-Length: {len(body)}\n"
+
+    # -- Build the message by joining together all the parts
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+
+# -------------- MAIN PROGRAM
+# ------ Configure the server
+# -- Listening socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Setup up the socket's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Become a listening socket
+ls.listen()
+
+print("SEQ Server configured!")
+
+# --- MAIN LOOP
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server Stopped!")
+        ls.close()
+        exit()
+    else:
+
+        # Service the client
+        process_client(cs)
+
+        # -- Close the socket
+        cs.close()
