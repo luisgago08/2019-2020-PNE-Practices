0ddf356bac86351b092c21d5558f29925d00681a
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-05-22 03:29:45 +0200

    Session 14

---
+++ Session-14/webserver-EX01.py
@@ -0,0 +1,80 @@
+import http.server
+import socketserver
+
+
+# Define the Server's port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        print(self.requestline)
+
+        req_line = self.requestline.split(" ")
+
+        # Get the path which always start with /
+        path = req_line[1]
+
+        # Para que cojatodo lo que está después de la /
+        path = path[1:]
+
+        contents =""
+
+        results = 0
+
+
+        if path == "":
+
+            # Message to send back to the clinet
+            contents = "Welcome to my server"
+            results = 200
+        else:
+            contents = "Resource not available"
+            results = 404
+
+
+        # Generating the response message
+        self.send_response(200)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
