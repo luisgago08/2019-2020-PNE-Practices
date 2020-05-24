09636d72169b837af11f89cdd2ae7e3b7a8aea19
luisgago08 <l.garciago.2019@alumnos.urjc.es>
2020-05-24 23:29:33 +0200

    Final Project

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -7,7 +7,7 @@ import json
 # -- THE PORT
 PORT = 8080
 Server = "rest.ensembl.org"
-Parameters = "?content-type=application/json"
+Params = "?content-type=application/json"
 
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
@@ -32,7 +32,10 @@ def server(Request_line):
     return data1
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
 # It means that our class inheritates all his methods and properties
+
+
 class TestHandler(http.server.BaseHTTPRequestHandler):
+
     def do_GET(self):
         """This method is called whenever the client invokes the GET method
         in the HTTP protocol request"""
@@ -46,7 +49,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         # Get the path. It always start with the / symbol
         path = req_line[1]
 
-        # separamos el path,  lo que está antes del ? y lo que está depues
+        # Separamos el path antes del ? y depués
         arguments = path.split("?")
 
         endpoint = arguments[0]
@@ -58,19 +61,19 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         elif endpoint == "/listSpecies":
             try:
                 # Get what is after ? (limit=10)
-                limit_number = arguments[1]
+                limit_numb = arguments[1]
                 # Get selected specie
-                limit = limit_number.split("=")[1]
+                limit = limit_numb.split("=")[1]
                 Endpoint = "/info/species"
                 # This is the req line to search the info
-                Request_line = Endpoint + Parameters
+                Request_line = Endpoint + Params
 
                 # Create a variable with the data, form the JSON received
                 name_specie = server(Request_line)["species"]
 
-                count_species = 0
+                count = 0
                 for element in name_specie:
-                    count_species = count_species + 1
+                    count = count + 1
                 contents = f""" 
                                     <!DOCTYPE html>
                                     <html lang = "en">
@@ -79,7 +82,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                                       <title> List of species </title >
                                     </head >
                                     <body style="background-color: lightblue;">
-                                    <p> The total number of species in the ensembl is: {count_species} </p>
+                                    <p> The total number of species in the ensembl is: {count} </p>
                                     </body>
                                     </html>
                                     """
@@ -87,7 +90,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     contents += f"""<a href="/">Main page</a>"""
                     status = 200
 
-                elif int(limit) > count_species:
+                elif int(limit) > count:
                     contents = Path('Error.html').read_text()
                     status = 404
                 else:
@@ -95,7 +98,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                                     <p> The name of the species are: </p>"""
 
                     status = 200
-                    for element in name_specie[:int(limit)]:  # From the beginning to the limit introduced by the client
+                    for element in name_specie[:int(limit)]:  # From beginning to limit
                         contents += f""" <p> - {element["common_name"]}</p>"""
                     contents += f"""<a href="/">Main page</a>"""
             except ValueError:
@@ -110,12 +113,12 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             name_specie = specie_name.split("=")[1]
             Endpoint = "/info/assembly/"
             # This is the req line to search the info
-            Request_line = Endpoint + name_specie + Parameters
+            Request_line = Endpoint + name_specie + Params
             try:
                 # Check if the req line is ok
                 Request_line.isidentifier()
-                # Create a variable with the data,form the JSON received
-                k_specie = server(Request_line)
+                # Create a variable with the data
+                kar_sp = server(Request_line)
                 contents = f""" 
                                    <!DOCTYPE html>
                                    <html lang = "en">
@@ -130,7 +133,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                                    </html> 
                                    """
                 status = 200
-                for element in k_specie["karyotype"]:
+                for element in kar_sp["karyotype"]:
                     contents += f"""<p>{element}</p>"""
                 contents += f"""<a href="/">Main page</a>"""
 
@@ -141,17 +144,17 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         # Option3
         elif endpoint == "/ChromLength":
             # Get what is after ? (specie=mouse&chromo=18)
-            specie_chromo = arguments[1]
+            sp_chromo = arguments[1]
             # Get the specie that we select
-            name_specie = specie_chromo.split("&")[0].split("=")[1]
-            number_chromo = specie_chromo.split("&")[1].split("=")[1]
+            name_specie = sp_chromo.split("&")[0].split("=")[1]
+            numb_chromo = sp_chromo.split("&")[1].split("=")[1]
             Endpoint = "/info/assembly/"
             # This is the req line to search the info
-            Request_line = Endpoint + name_specie + "/" + number_chromo + Parameters
+            Request_line = Endpoint + name_specie + "/" + numb_chromo + Params
             try:
                 # Check if the req line is ok
                 Request_line.isidentifier()
-                # Create a variable with the data, form the JSON received
+                # Create a variable with the data
                 l_chromosome = server(Request_line)
                 contents = f""" 
                                     <!DOCTYPE html>
@@ -200,5 +203,5 @@ with socketserver.TCPServer(("", PORT), Handler) as httpd:
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("")
-        print("Stoped by the user")
+        print("Stopped by the user")
         httpd.server_close()
